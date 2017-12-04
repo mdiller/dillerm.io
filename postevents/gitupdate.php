e1d611e00837736ea9a8e448350ef067fa749891
Malcolm Diller <dillerm@oregonstate.edu>
2017-12-03 20:25:10 -0800

    updated gitupdate to be able to run custom build scripts

--- postevents/gitupdate.php
+++ postevents/gitupdate.php
@@ -1,18 +1,27 @@
 <?php
-$git_paths = json_decode(file_get_contents("git_paths.json"), true);
+$git_repos = json_decode(file_get_contents("git_repos.json"), true);
 
 if (!empty($_REQUEST['payload'])) {
 	$data = json_decode($_REQUEST['payload'], true);
 	$repo = $data['repository']['full_name'];
-	if (array_key_exists($repo, $git_paths)) {
-		$path = $git_paths[$repo];
+	if (array_key_exists($repo, $git_repos)) {
+		$path = $git_repos[$repo]['path'];
+		if (array_key_exists('build', $git_repos[$repo])) {
+			$build_cmd = $git_repos[$repo]['build'];
+		}
 	}
 	else {
 		echo "invalid repository: " . $repo;
 		die(1);
 	}
+
+	$cmd = "cd " . $path . "; git pull;";
+
+	if (isset($build_cmd)) {
+		$cmd = $cmd . " " . $build_cmd . ";";
+	}
 	
-	$output = shell_exec("cd " . $path . "; git pull;");
+	$output = shell_exec($cmd);
 	
 	if(is_null($output)) {
 		echo "error executing pull";
